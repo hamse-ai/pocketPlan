08d7f53ccd11854a48977e5eeb0f7ca3bb9f08ef
Adeodatus Nkundimana <a.nkundiman@alustudent.com>
2026-03-12 19:50:26 +0200

    implement the text styles for the theme

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -6,15 +6,11 @@ class AppColors {
   static const Color onPrimarySecondary = Color(0xFFD9D9D9,); // For secondary text on primary color
   static const Color background = Color(0xFFFFFFFF); // For app background
   static const Color surface = Color(0xFFD9D9D9,); // For cards and surfaces background
-
-  // STATES
+  static const Color onSurface = Color(0xFF000000,); // For text on surface color
+  static const Color onSurfaceSecondary = Color(0xFF989898,); // For secondary text on surface color
   static const Color disabled = Color(0xFF737171); // For disabled toggle
-
-  // ICONS / NAVIGATION
   static const Color iconInactive = Color(0xFFCCCCCC,); // For inactive icons on the bottom navigation bar
   static const Color iconActive = Color(0xFF222222,); // For active icons on the bottom navigation bar
-
-  // FEEDBACK
   static const Color warning = Color(0xFFE57373); // For warning messages
   static const Color onWarning = Color(0xFFFFFFFF); // For text on warning messages
   static const Color onWarningSecondary = Color(0xFFDBDBDB); // For secondary text on warning messages
